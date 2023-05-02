f2bf685eb9bbd0fbc745a367ad2ff221537e3cb4
Vivek <vc1262461@gmail.com>
2023-05-02 20:26:33 +0530

    Added async await

--- src/features/project-list/api/fetchApis.js
+++ src/features/project-list/api/fetchApis.js
@@ -5,20 +5,20 @@ const categoryId = "category_id";
 const title = "title";
 const brief = "brief";
 
-export const fetchProjectList = () => {
-  return axios.get(endPoints.projectList);
+export const fetchProjectList = async () => {
+  return await axios.get(endPoints.projectList);
 };
 
-export const fetchProjectListFilter = ({ queryKey }) => {
+export const fetchProjectListFilter = async ({ queryKey }) => {
   const id = queryKey[1];
   const searchValue = queryKey[2];
 
   if (id && !searchValue) {
-    return axios.get(`${endPoints.projectList}?${categoryId}=${id}`);
+    return await axios.get(`${endPoints.projectList}?${categoryId}=${id}`);
   } else if (!id && searchValue) {
-    return axios.get(`${endPoints.projectList}?${title}_like=${searchValue}`);
+    return await axios.get(`${endPoints.projectList}?${title}_like=${searchValue}`);
   }
-  return axios.get(
+  return await axios.get(
     `${endPoints.projectList}?${title}_like=${searchValue}&${categoryId}=${id}`
   );
 };
--- src/features/user-project-list/api/fetchApis.js
+++ src/features/user-project-list/api/fetchApis.js
@@ -5,20 +5,22 @@ const categoryId = "category_id";
 const title = "title";
 const brief = "brief";
 
-export const fetchUserProjectList = () => {
-  return axios.get(endPoints.userProjectList);
+export const fetchUserProjectList = async () => {
+  return await axios.get(endPoints.userProjectList);
 };
 
-export const fetchUserProjectListFilter = ({ queryKey }) => {
+export const fetchUserProjectListFilter = async ({ queryKey }) => {
   const id = queryKey[1];
   const searchValue = queryKey[2];
 
   if (id && !searchValue) {
-    return axios.get(`${endPoints.userProjectList}?${categoryId}=${id}`);
+    return await axios.get(`${endPoints.userProjectList}?${categoryId}=${id}`);
   } else if (!id && searchValue) {
-    return axios.get(`${endPoints.userProjectList}?${title}_like=${searchValue}`);
+    return await axios.get(
+      `${endPoints.userProjectList}?${title}_like=${searchValue}`
+    );
   }
-  return axios.get(
+  return await axios.get(
     `${endPoints.userProjectList}?${title}_like=${searchValue}&${categoryId}=${id}`
   );
 };
